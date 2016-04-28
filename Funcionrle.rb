2f53a6482186d2c96b476b6b4077e861eea46e51
CaezadeShansho <d.vilches1234@gmail.com>
2016-04-27 23:00:28 -0300

    tarea completa

--- Funcionrle.rb
+++ Funcionrle.rb
@@ -12,41 +12,39 @@ class NodeRLE
 		@next_node = next_node
 		@counter = counter
 	end
-end
-def rlepush(head,val, cont)	
-	aux=NodeRLE.new(val, cont, head)
-	head=aux
 end	
 def rle(node)
 	aux=node
-	val=node.value
 	cont=0
-	while val==node.value
+	while aux.value==node.value
 		cont+=1
 		aux=aux.next_node	
 		val=aux.value
-	end		
-	head=NodeRLE.new(val, cont, nil)	
-	while node.next_node!=nil
+	end
+	head=NodeRLE.new(aux.value, cont, nil)	
+	while aux!=nil
 		aux2=aux
 		cont=0
-		val2=val
-		while val2==aux.value
+		while aux2.value==aux.value			
 			cont+=1
-			aux2=aux2.next_node
-			val2=aux2.value
+			if aux2.next_node==nil
+				break
+			else
+				aux2=aux2.next_node		
+			end	
 		end	
-		rlepush(head, val2, cont)
+		auxrle=	NodeRLE.new(aux2.value, cont, head)
+		head=auxrle
+		aux=aux.next_node
 	end	
 	return head
 end	
-
 a=Node.new(1,nil)
 b=Node.new(1,a)
 c=Node.new(3,b)
 d=Node.new(4,c)
 e=Node.new(1,d)
-
-puts rle(e).value
-puts rle(e).counter
-puts rle(e).next_node
+er=rle(e)
+puts er.value
+puts er.counter
+puts er.next_node
